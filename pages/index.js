90b647b369fb0c7384ee838388d6687078bb8d3d
Sonia <ortizt.sonia@gmail.com>
2022-09-09 18:54:00 -0500

    removed code

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Link from 'next/Link'
+import styles from '../styles/Home.module.css';
+
 export default function Home() {
   return (
     <div className={styles.container}>
