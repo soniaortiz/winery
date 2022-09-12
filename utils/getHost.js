36796c05a8bbb1b928a0b424b6ea2fd615246abb
Sonia <ortizt.sonia@gmail.com>
2022-09-12 18:12:13 -0500

    auth

--- utils/getHost.js
+++ utils/getHost.js
@@ -1,4 +1,4 @@
 export default function getHost (){
     const environment = process.env.NODE_ENV;
-    return environment === 'development' ? 'http://localhost:3000' : process.env.NEXTAUTH_URL;
+    return environment === 'development' ? 'http://localhost:3000' : process.env.NEXT_PUBLIC_VERCEL_URL;
 }
