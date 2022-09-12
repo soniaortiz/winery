7289b017a5d95d239982f75069ac4f29a82f81e7
Sonia <ortizt.sonia@gmail.com>
2022-09-12 15:54:20 -0500

    removed secret

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,7 +5,6 @@ import GithubProvider from "next-auth/providers/github"
 export default NextAuth({
     providers: [
         GithubProvider({
-            secret: process.env.SECRET,
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET
         }),
--- utils/getDBHost.js
+++ utils/getDBHost.js
@@ -1,7 +1,7 @@
 export default function getDBHost(){
     const environment = process.env.NODE_ENV;
 
-    return environment === 'development' ? 'mongodb://localhost:27017/winery' : 'mongodb+srv://satlasadmin:AtmiperritoB17@cluster0.v0kvhlq.mongodb.net/winery'
+    return 'mongodb://localhost:27017/winery';
 
 
 }
