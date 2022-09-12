943e8c83e142bce4540805fb1324506ced59783d
Sonia <ortizt.sonia@gmail.com>
2022-09-12 14:34:46 -0500

    added secret

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,8 +5,9 @@ import GithubProvider from "next-auth/providers/github"
 export default NextAuth({
     providers: [
         GithubProvider({
-            clientId: process.env.NEXT_PUBLIC_GITHUB_ID,
-            clientSecret: process.env.NEXT_PUBLIC_GITHUB_SECRET
+            secret: process.env.SECRET,
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET
         }),
 
     ]
