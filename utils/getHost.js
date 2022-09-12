81822c244064c1fcdd42ce48a07f9e5d61a84e20
Sonia <ortizt.sonia@gmail.com>
2022-09-12 18:09:39 -0500

    auth

--- utils/getHost.js
+++ utils/getHost.js
@@ -1,4 +1,4 @@
 export default function getHost (){
     const environment = process.env.NODE_ENV;
-    return environment === 'development' ? 'http://localhost:3000' : 'https://winery-nine.vercel.app'
+    return environment === 'development' ? 'http://localhost:3000' : process.env.NEXTAUTH_URL;
 }
