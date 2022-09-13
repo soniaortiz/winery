b2f3b26e744e119c8b3439768da4c99054254939
Sonia <ortizt.sonia@gmail.com>
2022-09-13 17:31:32 -0500

    added grapeType to schema

--- models/grape.js
+++ models/grape.js
@@ -2,7 +2,8 @@ import mongoose, {Schema}  from 'mongoose';
 
 const GrapeSchema = new Schema ({
     grapeName: {type: String, required: true, index: {unique: true}},
-    grapeDescription: {type: String, required: true}
+    grapeDescription: {type: String, required: true},
+    grapeType: {type: String, required: true}
 })
 
 module.exports = mongoose.models.Grape || mongoose.model('Grape', GrapeSchema);
