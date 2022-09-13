41f858f91827e2dc654b704ae6f07c98f5257dbb
Sonia <ortizt.sonia@gmail.com>
2022-09-12 22:48:21 -0500

    fixed styles

--- components/header.js
+++ components/header.js
@@ -5,48 +5,53 @@ export default function Header(){
     const  {session, status}  = useSession();
 
     return (
-        <div className="headerStyle">
-                <Link href={'/'} >
-                    <a className='buttonMenu'>
-                        Home
-                    </a>
+        <div className="headerStyle" >
+            <Link href={'/'} >
+                <a className={'buttonMenu'}>
+                    Home
+                </a>
 
-                </Link>
+            </Link>
 
-                <Link href={'/admin/grape/grapes-list'}>
-                    <a className={'buttonMenu'}>
-                        Grapes List
-                    </a>
-                    
-                </Link>
+            <Link href={'/admin/grape/grapes-list'}>
+                <a className={'buttonMenu'}>
+                    Grapes List
+                </a>
                 
-                <Link href={'/admin/grape/add-grape'}>
-                    <a className={'buttonMenu'}>
-                        Add Grape
-                    </a>
-                </Link>
+            </Link>
+            
+            <Link href={'/admin/grape/add-grape'}>
+                <a className={'buttonMenu'}>
+                    Add Grape
+                </a>
+            </Link>
+            
+            {(!session && status==='unauthenticated') && <Link href={'/api/auth/sign-in'} >
+                <a onClick={
+                (e)=>{
+                    e.preventDefault();
+                    signIn();
+                }
                 
-                {(!session && status==='unauthenticated') && <Link href={'/api/auth/sign-in'} >
-                    <a className={'buttonMenu'} onClick={
-                    (e)=>{
-                        e.preventDefault();
-                        signIn();
-                    }
-                }>
-                        Sign in
-                    </a>
-                </Link>}
+            }
+                className={'buttonMenu'}
+            >
+                    Sign in
+                </a>
+            </Link>}
 
-                {(status === 'authenticated') && <Link href={'/api/auth/sign-out'} >
-                    <a className={'buttonMenu'} onClick={
-                    (e)=>{
-                        e.preventDefault();
-                        signOut();
-                    }
-                }>
-                        Sign out
-                    </a>
-                </Link>}
-        </div>
+            {(status === 'authenticated') && <Link href={'/api/auth/sign-out'} >
+                <a  onClick={
+                (e)=>{
+                    e.preventDefault();
+                    signOut();
+                }
+            }
+                className={'buttonMenu'}
+            >
+                    Sign out
+                </a>
+            </Link>}
+    </div>
     )
 }
