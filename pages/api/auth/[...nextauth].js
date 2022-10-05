fbc5d8c032db14fc4957aa552ac506c6214bb947
Sonia <ortizt.sonia@gmail.com>
2022-10-05 13:02:44 -0500

    added facebook and google auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,23 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
-// import FacebookProvider from 'next-auth/providers/facebook';
+import FacebookProvider from 'next-auth/providers/facebook';
+import GoogleProvider from 'next-auth/providers/google';
 
 export default NextAuth({
     providers: [
         GithubProvider({
             clientId: process.env.NEXTAUTH_GITHUB_ID,
             clientSecret: process.env.NEXTAUTH_GITHUB_SECRET
+        }),
+        FacebookProvider({
+            clientId: process.env.NEXTAUTH_FACEBOOK_ID,
+            clientSecret: process.env.NEXTAUTH_FACEBOOK_SECRET
+        }),
+        GoogleProvider({
+            clientId: process.env.NEXTAUTH_GOOGLE_ID,
+            clientSecret: process.env.NEXTAUTH_GOOGLE_SECRET            
         })
+
     ],
     secret: process.env.NEXTAUTH_SECRET
 })
