9b4ba14923b3de844e7d8e5d143d4cc9ebeab046
Sonia <ortizt.sonia@gmail.com>
2023-01-10 14:34:23 -0600

    [NO-JIRA]: added grapeType to db

--- pages/api/grape/new-grape/index.js
+++ pages/api/grape/new-grape/index.js
@@ -5,7 +5,7 @@ export default async function SaveGrape(req, res){
     try{
         const client = await connectMongo();
         const {grapeDescription, grapeName} = JSON.parse(req.body);
-        const newGrape = new Grape({grapeDescription, grapeName});
+        const newGrape = new Grape({grapeDescription, grapeName, grapeType: 1});
         const a = await newGrape.save();
         client.disconnect();
         res.send({status: 200, message: 'New grape added'});
